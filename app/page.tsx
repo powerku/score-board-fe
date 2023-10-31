d4c5b85c7e592b0f2b7ca0b77f64f7d8da64ed3e
griffin <griffin.ku@kakaocorp.com>
2023-10-31 10:46:56 +0900

    add: theme-toggle 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
+import {ModeToggle} from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-      <main>Hello</main>
+      <main>
+          <ModeToggle></ModeToggle>
+      </main>
       )
 }
